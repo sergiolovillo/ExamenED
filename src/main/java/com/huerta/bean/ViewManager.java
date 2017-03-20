e8fe3295d8cf5afd7d9bc7f88e2a228fd786aceb
sergiolovillo <sergiolovillo@outlook.com>
2017-03-20 20:21:28 +0100

    Add files via upload

---
+++ src/main/java/com/huerta/bean/ViewManager.java
@@ -0,0 +1,42 @@
+package com.huerta.bean;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+//import com.huerta.model.Property;
+
+@ManagedBean(name="obj")
+@RequestScoped
+public class ViewManager implements Serializable{
+//private Property item = new Property();
+  private String key;
+  private String value;
+  private String mensaje;
+
+  public String getMensaje() {
+    return mensaje;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+  
+  public String add() {
+	    System.out.println("Key Introducida:" + this.key + " Valor asociado:" + this.value + ".");
+	    setValue(this.value + "==");
+	    this.mensaje="Alta realizada";
+	    return "vista2";
+	  }
+}
